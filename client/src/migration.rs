33c395f31252693d6129b0232550932e74ac52ff
Mason <109301662+NativeCognitive@users.noreply.github.com>
2023-08-17 22:24:58 -0400

    Init 'client' workspace

---
+++ client/src/migration.rs
@@ -0,0 +1,12 @@
+/* -- Needs implementation
+
+#[cfg(target_os = "windows")]
+pub fn process_injection(pid: &str) {
+
+}
+*/
+
+#[cfg(target_os = "linux")]
+pub fn process_injection(pid: &str) {
+
+}
