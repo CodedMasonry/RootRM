ad94bce2c054aa6bc0918010ba8373f9f26bcbf9
Mason <109301662+NativeCognitive@users.noreply.github.com>
2023-08-18 21:00:45 -0400

    Add process creation for process injection

--- client/src/migration.rs
+++ client/src/migration.rs
@@ -1,12 +1,10 @@
-/* -- Needs implementation
+#[cfg(target_os = "windows")]
+use windows;
 
 #[cfg(target_os = "windows")]
 pub fn process_injection(pid: &str) {
-
+    let process = windows::Win32::System::Threading::OpenProcess(PROCESS_ACCESS_RIGHTS, false, pid);
 }
-*/
 
 #[cfg(target_os = "linux")]
-pub fn process_injection(pid: &str) {
-
-}
+pub fn process_injection(pid: &str) {}
